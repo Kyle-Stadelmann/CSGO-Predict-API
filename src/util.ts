47a3ea366728a8f591ddba0e953d5d51b36dda67
Kyle-Stadelmann <42564445+Kyle-Stadelmann@users.noreply.github.com>
2022-10-29 20:04:29 -0700

    Error fix again

--- src/util.ts
+++ src/util.ts
@@ -1,5 +1,5 @@
 export function generateError(e: any): Error {
-    if (e.response?.status || e.response?.statusText || e.response?.data) return e;
+    if (!e.response?.status || !e.response?.statusText || !e.response?.data) return e;
 
     const { status, statusText, data } = e.response;
 	return new Error(`${status}: ${statusText}\n${data}`);
