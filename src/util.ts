79eb45377a87c9294bc28cf962b1b505ab926072
Kyle-Stadelmann <42564445+Kyle-Stadelmann@users.noreply.github.com>
2022-10-29 20:03:15 -0700

    Error refactor

--- src/util.ts
+++ src/util.ts
@@ -1,4 +1,6 @@
 export function generateError(e: any): Error {
+    if (e.response?.status || e.response?.statusText || e.response?.data) return e;
+
     const { status, statusText, data } = e.response;
 	return new Error(`${status}: ${statusText}\n${data}`);
 }
