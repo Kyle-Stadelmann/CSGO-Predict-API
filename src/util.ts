002a44154714459c87e0a8c8b5f797c3c0ea2741
Kyle-Stadelmann <42564445+Kyle-Stadelmann@users.noreply.github.com>
2022-10-29 18:28:57 -0700

    Small bugfix

--- src/util.ts
+++ src/util.ts
@@ -1,5 +1,4 @@
 export function generateError(e: any): Error {
-	const { statusCode, statusText, data } = e;
-
-	return new Error(`${statusCode}:${statusText}\n${data}`);
+    const { status, statusText, data } = e.response;
+	return new Error(`${status}:${statusText}\n${data}`);
 }
